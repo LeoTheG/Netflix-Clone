a80e47eab750262c8a51edef1030312f76939763
LeoTheG <leonargharib@gmail.com>
2018-08-14 11:29:13 -0700

    Initial commit. Minimum viable webpack/babel/react setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(
+  <h1>Hey Man</h1>,
+  document.getElementById('app'),
+);
+if (module.hot) {
+  module.hot.accept();
+}
